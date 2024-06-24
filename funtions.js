294f662e5a814d1a716275c07c39147895eb38cd
AEscobarAngola <abrancho1908@gmail.com>
2024-06-24 12:18:36 +0200

    feat: funtions add: ifNum, strLength, promNum, working correctly

--- funtions.js
+++ funtions.js
@@ -13,7 +13,28 @@ var greatest = function (num1, num2) {
     }
     return "".concat(num2, " es mayor");
 };
+//// Reto 3
+var ifNum = function (num) {
+    if (num > 0) {
+        return "Es Positivo";
+    }
+    else if (num === 0) {
+        return "Es cero";
+    }
+    return "Es negativo";
+};
+//// Reto 4
+var strLength = function (str) {
+    return str.length === 5 ? "tiene 5 caracteres" : "tiene ".concat(str.length, " caracteres");
+};
+//// Reto 5
+var promNum = function (num1, num2, num3) {
+    return (num1 + num2 + num3) / 3 > 5 ? "El promedio es mayor a 5" : "El promedio es ".concat(((num1 + num2 + num3) / 3).toFixed(2));
+};
 module.exports = {
     multiplos: multiplos,
     greatest: greatest,
+    ifNum: ifNum,
+    strLength: strLength,
+    promNum: promNum,
 };
--- funtions.ts
+++ funtions.ts
@@ -9,18 +9,44 @@ const multiplos = (num1: number, num2: number): boolean => {
 
 //// Reto 2
 
-const greatest = (num1 : number, num2 : number) : string => {
-    if(num1 > num2){
+const greatest = (num1: number, num2: number): string => {
+    if (num1 > num2) {
         return `${num1} es mayor`
-    } else if(num1 === num2){
+    } else if (num1 === num2) {
         return `Son iguales`
     } return `${num2} es mayor`
 }
 
+//// Reto 3
+
+const ifNum = (num : number) : string => {
+    if(num > 0){
+        return `Es Positivo`
+    } else if(num === 0){
+        return `Es cero`
+    } return `Es negativo`
+}
+
+//// Reto 4
+
+const strLength = (str : string): string => {
+    return str.length === 5 ? `tiene 5 caracteres` : `tiene ${str.length} caracteres`
+}
+
+//// Reto 5
+
+const promNum = (num1: number, num2 : number, num3 : number) : string => {
+    return (num1 + num2 + num3) / 3 > 5 ? `El promedio es mayor a 5` : `El promedio es ${((num1 + num2 + num3) / 3).toFixed(2)}`
+}
+
+//// Reto 6
 
 module.exports = {
     multiplos,
     greatest,
+    ifNum,
+    strLength,
+    promNum,
 }
 
 
